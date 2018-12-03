2f52a5bd206573cd4794d29964418f282fc02f45
danilchican <danilchican@mail.ru>
2018-12-03 14:46:39 +0300

    Fixed dashboard top menu

--- resources/views/partials/dashboard/top_links.blade.php
+++ resources/views/partials/dashboard/top_links.blade.php
@@ -5,7 +5,6 @@
 </a>
 <ul class="dropdown-menu dropdown-usermenu pull-right">
     <li><a href="{{ route('dashboard.index') }}">@lang('dashboard.actions.dashboard')</a></li>
-    <li><a href="{{ route('profile.index') }}">@lang('dashboard.actions.profile')</a></li>
     <li><a href="{{ route('logout') }}"
            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
             <i class="fa fa-sign-out pull-right"></i> @lang('dashboard.actions.logout')

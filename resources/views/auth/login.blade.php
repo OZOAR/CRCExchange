2c790857b30cc0862972689106cd498c82a9b9d4
danilchican <danilchican@mail.ru>
2019-01-14 20:53:15 +0300

    Removed old custom styles file and moved it into sass folder.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.app_new')
 
 @section('content')
 <div class="container">
--- resources/views/layouts/app_new.blade.php
+++ resources/views/layouts/app_new.blade.php
@@ -7,12 +7,10 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'CRCExchange') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 </head>
 <body>
 @yield('content')

35b7d99c78156a059cc713e107cc3937c32208d9
PrimeForce <zaharko.19988@gmail.com>
2019-05-01 21:42:27 +0300

    Add footer

--- resources/views/partials/footer_new.blade.php
+++ resources/views/partials/footer_new.blade.php
@@ -1,4 +1,76 @@
-<footer>
+<!--footer start-->
+<footer class="app-footer">
+    <div class="primary-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 wow fadeInUp" data-wow-delay=".2s">
+                    <h4 class="text-uppercase">Kryptix</h4>
+                    <p>Subscribe to our newsletter to receive news, updates, free stuff and new releases by email. We don't do spam.</p>
+                    <p class="copyright">&copy; Copyright 2018 VectorLab, Inc.™</p>
+                </div>
+                <div class="col-md-4 mb-md-0 mb-3 wow fadeInUp" data-wow-delay=".3s">
+                    <h6 class="text-uppercase">Resources</h6>
+                    <ul class="list-unstyled">
+                        <li><a href="javascript:;">Free schedules</a></li>
+                        <li><a href="javascript:;">terms & privacy</a></li>
+                        <li><a href="javascript:;">api reference</a></li>
+                        <li><a href="javascript:;">help desk</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4 wow fadeInUp" data-wow-delay=".4s">
+                    <h6 class="text-uppercase">About & Contacts</h6>
+                    <ul class="list-unstyled">
+                        <li><a href="javascript:;">Our Vision</a></li>
+                        <li><a href="javascript:;">Features</a></li>
+                        <li><a href="javascript:;">About Us</a></li>
+                        <li><a href="javascript:;">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="secondary-footer text-md-left text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 mb-md-0 mb-3">
+                    <div class="payment-cards wow fadeInUp" data-wow-delay=".45s">
+                        <img src="assets/img/fc1.png" alt=""/>
+                        <img src="assets/img/fc2.png" alt=""/>
+                        <img src="assets/img/fc3.png" alt=""/>
+                        <img src="assets/img/fc4.png" alt=""/>
+                    </div>
+                </div>
+                <div class="col-md-6 text-md-right wow fadeInUp" data-wow-delay=".5s">
+                    <div class="float-md-right  mb-md-0 mb-3">
+                        <span class="sm-txt pr-2">Language:</span>
+                        <div class="btn-group ">
+                            <button type="button" class="btn-language dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                English
+                            </button>
+                            <div class="dropdown-menu dropdown-menu-right">
+                                <button class="dropdown-item" type="button">English</button>
+                                <button class="dropdown-item" type="button">French</button>
+                                <button class="dropdown-item" type="button">Chinese</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="social-links float-md-right">
+                        <a href="javascript:;"><i class="fa fa-facebook"></i></a>
+                        <a href="javascript:;"><i class="fa fa-twitter"></i></a>
+                        <a href="javascript:;"><i class="fa fa-bitcoin"></i></a>
+                        <a href="javascript:;"><i class="fa fa-youtube"></i></a>
+                        <a href="javascript:;"><i class="fa fa-linkedin"></i></a>
+                        <a href="javascript:;"><i class="fa fa-google-plus"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--footer end-->
+
+<!-- <footer>
     <div class="container">
         <a href="/terms-of-use" class="footer-link">@lang('homepage.footer.menu.terms')</a>
         <a href="{{ route('page.policy') }}" class="footer-link">@lang('homepage.footer.menu.policy')</a>
@@ -6,4 +78,4 @@
 
         <span id="footer-copy">&copy <script>document.write(new Date().getFullYear())</script> exrate.cc</span>
     </div>
-</footer>
+</footer> -->

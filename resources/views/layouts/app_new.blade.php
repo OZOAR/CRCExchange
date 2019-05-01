6cd35b91f6e8c51148f58664098212dccf089b7b
PrimeForce <zaharko.19988@gmail.com>
2019-05-01 22:06:46 +0300

    Add animate.css

--- resources/views/layouts/app_new.blade.php
+++ resources/views/layouts/app_new.blade.php
@@ -11,7 +11,6 @@
     <title>{{ config('app.name', 'CRCExchange') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>

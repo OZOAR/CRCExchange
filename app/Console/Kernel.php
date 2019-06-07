bcf3d1a9e1815e5612357821b633a48af9cb3008
danilchican <danilchican@mail.ru>
2019-06-07 15:29:43 +0300

    Updated kernel.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,8 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(UpdateCoursesCommand::class)
-            ->everyTenMinutes()
+        $schedule->command('courses:update')
             ->runInBackground()
             ->withoutOverlapping()
             ->appendOutputTo(storage_path() . '/logs/scheduler.log');
